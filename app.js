9f070555750e1239a3cf531072df436edbbd7192
seanbecker15 <sean.becker15@gmail.com>
2017-08-26 18:32:24 -0400

    env fix hopefully

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var EventHandler = require('./controllers/events');
 var bodyParser = require('body-parser'); // allows us to parse req.body in js files
 app.use(bodyParser.json()); // to support JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+dotenv.load();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
