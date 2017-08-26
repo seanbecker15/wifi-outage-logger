7be0c89927fc9bac71c6c36d2ec31471ff3640d3
seanbecker15 <sean.becker15@gmail.com>
2017-08-26 18:37:25 -0400

    Another try with the env

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var EventHandler = require('./controllers/events');
 var bodyParser = require('body-parser'); // allows us to parse req.body in js files
 app.use(bodyParser.json()); // to support JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+var dotenv = require('dotenv');
 dotenv.load();
 
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
@@ -16,8 +18,8 @@ app.use(express.static('.'));
 app.get('/', function(req, res) { res.render('index'); });
 app.post('/', EventHandler.onSubmit);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express server listening on port ', this.address().PORT);
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ', app.get('port'));
 });
 
 module.exports = app;
